d375a6104a35d417702a1d8cf5a2c970356b2679
Artem Batohov <artem.batog@gmail.com>
2023-10-19 08:42:08 -0700

    first commit

---
+++ slice.go
@@ -0,0 +1,36 @@
+package main
+
+// import "fmt"
+
+// func Slice() {
+// 	var s []int
+// 	printSlice(s)
+
+// 	// append працює на 'nil' зрізах.
+// 	s = append(s, 0)
+// 	printSlice(s)
+
+// 	//  Зріз росте за потреби.
+// 	s = append(s, 1)
+// 	printSlice(s)
+
+// 	// Можна додавати більше одного елемента за раз.
+// 	s = append(s, 2, 3, 4)
+// 	printSlice(s)
+// }
+
+// func printSlice(s []int) {
+// 	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+// }
+
+// package main
+
+// import "fmt"
+
+// var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
+// func main() {
+// 	for i, v := range pow {
+// 		fmt.Printf("2**%d = %d\n", i, v)
+// 	}
+// }
